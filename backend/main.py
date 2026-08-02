28f020072457f2c6bba80f04a8c3eafed421a523
Sourabh Singh <sourabh3527@gmail.com>
2026-08-02 10:44:41 +0530

    Add '/ping' endpoint to check service status

--- backend/main.py
+++ backend/main.py
@@ -60,3 +60,7 @@ def read_root():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
